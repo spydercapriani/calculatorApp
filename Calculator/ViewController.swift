6f74f734c41dc60a5eb13f0ad44893b6e790f2f9
Danny Gilbert <gilbert.daniel@heb.com>
2016-01-08 14:59:54 -0600

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Daniel Gilbert on 1/8/16.
+//  Copyright © 2016 Daniel Gilbert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
